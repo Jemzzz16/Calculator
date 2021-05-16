b2e65937a75e10c8245ac379ffc907720c5a6871
Jemma Holmes <jemmaholmes@jemmas-mbp.home>
2021-05-16 12:25:54 +0100

    numbers display on calculator

--- main.js
+++ main.js
@@ -1,7 +1,33 @@
-const numberButtons = document.querySelectorAll("[data-number]");
-const operationButtons = document.querySelectorAll("[data-operation]");
-const equalButtons = document.querySelector("[data-equal]");
-const clearButtons = document.querySelector("[data-clear]");
-const operandButtons = document.querySelector("[data-operand]");
+// const numberButtons = document.querySelectorAll("[data-number]");
+// const operationButtons = document.querySelectorAll("[data-operation]");
+// const equalButtons = document.querySelector("[data-equal]");
+// const clearButtons = document.querySelector("[data-clear]");
+// const operandButtons = document.querySelector("[data-operand]");
 
-const calculator = document.querySelector("calculator");
+const display = document.querySelector('.calculator__display')
+const clear = document.querySelector('.clear')
+
+const keys = document.querySelector(".calculator__keys");
+
+keys.addEventListener('click', event => {
+    if (!event.target.closest('button')) return
+
+    const key = event.target
+    const keyValue = key.textContent
+    const displayValue = display.textContent
+
+    // number key
+   if (key.classList.contains ('number')) { 
+    if (displayValue === '0') {
+        display.textContent = keyValue 
+     } else {
+        display.textContent = displayValue + keyValue
+        };
+    }   
+        
+    // operator key
+    if (key.classList.contains ('operator')) {
+        console.log(key);
+    }
+
+}); 
