fc26f1dd05b9adb969196424a7cf73cae803dc43
Jemma Holmes <jemmaholmes@jemmas-mbp.home>
2021-05-17 20:43:32 +0100

    Keys now display

--- main.js
+++ main.js
@@ -5,13 +5,26 @@ const calculator = document.querySelector('.calculator')
 const clear = document.querySelector('.clear')
 const equals = document.querySelector('.equal')
 
+////////// calculator -> (1) listen for all key presses(2) determine the type of key
 
 keys.addEventListener('click', event => {
-    const key = event.target
-    const type = event.target 
+    const opKey = event.target  //operator
+    const numType = event.target  //number
+    const displayedNum = display.textContent //displaying numbers selected 
+    const keyValue = opKey.textContent //key value is the number key selected
 
-    console.log(type.innerHTML);  
-    console.log({type});  
-}); 
+    // DISPLAY NUMBERS - this allows for number to be displayed and subsequent numbers too
+    if (displayedNum === '0'|| previousKeyType === 'operator') { 
+        display.textContent = keyValue
+    } else {
+        display.textContent = displayedNum + keyValue
 
-//display.addEventListener()
+    } if (numType === 'decimal') {
+        display.textContent = displayedNum + '.'
+        calculator.dataset.previousKeyType = 'operator'
+    }
+
+    console.log(numType.innerHTML);  
+    console.log({numType});  
+        
+});
